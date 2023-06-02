30deac04366c706aabc0f082e0433468230b903e
iamsam404 <131537226+iamsam404@users.noreply.github.com>
2023-06-02 21:38:02 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -95,10 +95,8 @@ const Post: React.FC<PostProps> = (props) => {
 					property="og:image:alt"
 					content={post.featuredImage.node.altText}
 				/>
-				<title>{post.title}</title>
 			</Head>
 			<div className="post-container">
-				<h1>{post.title}</h1>
 				<img
 					src={post.featuredImage.node.sourceUrl}
 					alt={post.featuredImage.node.altText}
